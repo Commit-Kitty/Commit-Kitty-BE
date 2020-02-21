d8959d30203bd01d472d2ee12a7ea0cf203a9177
joelpdurham <joeldur@gmail.com>
2020-02-21 11:11:41 -0800

    we can write routes like adults

---
+++ __tests__/callback.test.js
@@ -0,0 +1,28 @@
+require('dotenv').config();
+
+const request = require('supertest');
+const app = require('../lib/app');
+const connect = require('../lib/utils/connect');
+const mongoose = require('mongoose');
+
+describe('app routes', () => {
+  beforeAll(() => {
+    connect();
+  });
+
+  beforeEach(() => {
+    return mongoose.connection.dropDatabase();
+  });
+
+  afterAll(() => {
+    return mongoose.connection.close();
+  });
+
+  it('can send we made it', async() => {
+    await request(app)
+      .get('/api/v1/callback')
+      .then(res => {
+        expect(res.text).toEqual('We made it!');
+      });
+  });
+});
