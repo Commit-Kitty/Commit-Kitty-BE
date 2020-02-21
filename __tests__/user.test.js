87a0609e2f717e13ae98e01fdf43f768ef4d6a12
joelpdurham <joeldur@gmail.com>
2020-02-21 09:26:26 -0800

    wrote a failing user signup route test

---
+++ __tests__/user.test.js
@@ -0,0 +1,41 @@
+require('dotenv').config();
+
+const request = require('supertest');
+const app = require('../lib/app');
+const connect = require('../lib/utils/connect');
+const mongoose = require('mongoose');
+const User = require('../lib/models/User');
+
+describe('app routes', () => {
+  beforeAll(() => {
+    connect();
+  });
+
+  beforeEach(() => {
+    return mongoose.connection.dropDatabase();
+  });
+
+  afterAll(() => {
+    return mongoose.connection.close();
+  });
+
+  it('can signup a user', () => {
+    return request(app)
+      .post('/api/v1/auth/signup')
+      .send({
+        userName: 'joel',
+        email: 'joel@joel.com',
+        password: '1234',
+        role: 'Admin'
+      })
+      .then(res => {
+        expect(res.body).toEqual({
+          _id: expect.any(String),
+          userName: 'joel',
+          email: 'joel@joel.com',
+          role: 'Admin',
+          __v: 0
+        });
+      });
+  });
+});
