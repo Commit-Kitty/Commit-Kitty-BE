7b337c2f83df4c2fcb69358eb846dbc11e4fda2f
Aaron Glenn <AaronEdwardGlenn@gmail.com>
2020-02-21 12:01:51 -0800

    added teh invalid password and email tests

--- __tests__/user.test.js
+++ __tests__/user.test.js
@@ -4,6 +4,7 @@ const request = require('supertest');
 const app = require('../lib/app');
 
 describe('app routes', () => {
+
   it('can signup a user', () => {
     return request(app)
       .post('/api/v1/auth/signup')
@@ -44,4 +45,32 @@ describe('app routes', () => {
       });
   });
 
+
+  it('fails to login a user with a bad email', async() => {
+    await getUser();
+    return request(app)
+      .post('/api/v1/auth/login')
+      .send({ email: 'badEmail@notgood.io', password: 'password' })
+      .then(res => {
+        expect(res.status).toEqual(401);
+        expect(res.body).toEqual({
+          status: 401,
+          message: 'Invalid Email or Password'
+        });
+      });
+  });
+});
+    
+it('fails to login a user with a bad password', async() => {
+  await getUser();
+  return request(app)
+    .post('/api/v1/auth/login')
+    .send({ email: 'user@tess.com', password: 'notright' })
+    .then(res => {
+      expect(res.status).toEqual(401);
+      expect(res.body).toEqual({
+        status: 401,
+        message: 'Invalid Email or Password'
+      });
+    });
 });
