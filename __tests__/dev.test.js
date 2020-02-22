613e5d30a91155598ec070110ba44010915e7c11
Tess Lameyer <tjlameyer@gmail.com>
2020-02-21 17:24:52 -0800

    ready to TDD for Dev CREATE route

---
+++ __tests__/dev.test.js
@@ -0,0 +1,27 @@
+require('dotenv').config();
+const { getDevs, devAgent } = require('../lib/helpers/data-helpers');
+const request = require('supertest');
+const app = require('../lib/app');
+
+
+describe('tests of dev model routes', () => {
+  it('can create a dev with ensureAuth only (either Dev or Admin)', () => {
+    return devAgent
+      .post('/api/v1/dev/')
+      .send({
+        devName: 'great group',
+        devGitHubHandle: '@githubHandle',
+      })
+      .then(res => {
+        expect(res.body).toEqual({
+          _id: expect.any(String),
+          devName: 'great group',
+          devGitHubHandle: '@githubHandle',
+          __v: 0
+        });
+      });
+  });
+  
+
+
+});
