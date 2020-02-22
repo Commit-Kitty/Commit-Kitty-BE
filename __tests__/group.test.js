91630902c7175f24920e89ba8ac1600a717cb9f8
Tess Lameyer <tjlameyer@gmail.com>
2020-02-21 17:09:04 -0800

    miss me with that cookie, test for group model POST is passing

--- __tests__/group.test.js
+++ __tests__/group.test.js
@@ -17,7 +17,6 @@ describe('tests of group model routes', () => {
         devsInGroup: [devs[0]._id, devs[1]._id]
       })
       .then(res => {
-        expect(res.header['set-cookie'][0]).toEqual(expect.stringContaining('session='));
         expect(res.body).toEqual({
           _id: expect.any(String),
           groupName: 'great group',
