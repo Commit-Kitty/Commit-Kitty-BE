9496fa57345bb1cda7838b9fc24c7d692a4638d8
Tess Lameyer <tjlameyer@gmail.com>
2020-02-21 17:03:50 -0800

    ready to TDD POST route for Group model

--- __tests__/group.test.js
+++ __tests__/group.test.js
@@ -1,32 +1,32 @@
 require('dotenv').config();
-const { getGroup, getDevs, devAgent, adminAgent } = require('../lib/helpers/data-helpers');
+const { getGroup, getUser, getDevs, adminAgent } = require('../lib/helpers/data-helpers');
 const request = require('supertest');
 const app = require('../lib/app');
 
 
 describe('tests of group model routes', () => {
   it('can create a group with admin access only ', async() => {
+    const admin = await getUser({ email: 'admin0@tess.com' });
     const devs = await getDevs();
 
-    return request(app)
+    return adminAgent
       .post('/api/v1/group/')
       .send({
         groupName: 'great group',
-        adminId: '1234',
+        adminId: admin._id,
         devsInGroup: [devs[0]._id, devs[1]._id]
       })
       .then(res => {
         expect(res.header['set-cookie'][0]).toEqual(expect.stringContaining('session='));
         expect(res.body).toEqual({
           _id: expect.any(String),
-          userName: 'joel',
-          email: 'joel@joel.com',
-          role: 'Admin',
+          groupName: 'great group',
+          adminId: admin._id.toString(),
+          devsInGroup: [devs[0]._id.toString(), devs[1]._id.toString()],
           __v: 0
         });
       });
   });
-
   
 
 
