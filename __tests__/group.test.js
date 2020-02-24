bec2bd2f420ef5cbe6ec7e21ef3f76d251eb8b55
Aaron Glenn <AaronEdwardGlenn@gmail.com>
2020-02-24 13:42:42 -0800

    can get a group id with ensureAdmin test passes

--- __tests__/group.test.js
+++ __tests__/group.test.js
@@ -5,9 +5,8 @@ const app = require('../lib/app');
 
 
 describe('tests of group model routes', () => {
-  it.only('can create a group with admin access only ', async() => {
+  it('can create a group with admin access only ', async() => {
     const admin = await getUser({ email: 'admin0@tess.com' });
-    // const devs = await getDevs(dev => dev.indexOf(0) || dev.indexOf(1));
     const dev1 = await getDev({ devGitHubHandle: '@devHandle0' });
     const dev2 = await getDev({ devGitHubHandle: '@devHandle1' });
 
@@ -29,32 +28,27 @@ describe('tests of group model routes', () => {
       });
   });
 
-  // it('can get a group by id with ensureAdminAuth', async() => {
-  //   const admin = await getUser({ email: 'admin0@tess.com' });
-  //   const devs = await getDevs();
-  //   const group = await getGroup();
+  it('can get a group by id with ensureAdminAuth', async() => {
+    const admin = await getUser({ email: 'admin0@tess.com' });
+    const group = await getGroup({ groupName: 'groupName0' });
 
-  //   return adminAgent
-  //     .get(`/api/v1/group/${group._id}`)
-  //     .send({
-  //       groupName: group.groupName,
-  //       adminIds: [
-  //         admin._id,
-  //       ],
-  //       devsInGroup: [devs[0]._id, devs[1]._id]
-  //     })
-  //     .then(res => {
-  //       expect(res.body).toEqual({
-  //         _id: expect.any(String),
-  //         groupName: group.groupName,
-  //         adminIds: [
-  //           admin._id.toString(),
-  //         ],
-  //         devsInGroup: [devs[0]._id.toString(), devs[1]._id.toString()],
-  //         __v: 0
-  //       });
-  //     });
-  // });
+    return adminAgent
+      .get(`/api/v1/group/${group._id}`)
+      .send({
+        groupName: group.groupName,
+        adminIds: [admin._id],
+        devsInGroup: [group.devsInGroup[0]._id, group.devsInGroup[1]._id]
+      })
+      .then(res => {        
+        expect(res.body).toEqual({
+          _id: expect.any(String),
+          groupName: group.groupName,
+          adminIds: [admin._id.toString()],
+          devsInGroup: expect.any(Array),
+          __v: 0
+        });
+      });
+  });
 
 
 });
