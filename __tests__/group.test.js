56b5a09858deeb44077a4b85b89078eca51db11b
Aaron Glenn <AaronEdwardGlenn@gmail.com>
2020-02-24 11:40:39 -0800

    added our failing get group by id route with route creation to follow

--- __tests__/group.test.js
+++ __tests__/group.test.js
@@ -13,20 +13,46 @@ describe('tests of group model routes', () => {
       .post('/api/v1/group/')
       .send({
         groupName: 'great group',
-        adminId: admin._id,
+        adminIds: [admin._id],
         devsInGroup: [devs[0]._id, devs[1]._id]
       })
       .then(res => {
         expect(res.body).toEqual({
           _id: expect.any(String),
           groupName: 'great group',
-          adminId: admin._id.toString(),
+          adminIds: [admin._id.toString()],
           devsInGroup: [devs[0]._id.toString(), devs[1]._id.toString()],
           __v: 0
         });
       });
   });
-  
+
+  it('can get a group by id with ensureAdminAuth', async() => {
+    const group = await getGroup();
+    const admin = await getUser({ email: 'admin1@tess.com' });
+    const devs = await getDevs();
+
+    return adminAgent
+      .get(`/api/v1/group/${group._id}`)
+      .send({
+        groupName: group.groupName,
+        adminIds: [
+          admin._id,
+        ],
+        devsInGroup: [devs[0]._id, devs[1]._id]
+      })
+      .then(res => {
+        expect(res.body).toEqual({
+          _id: expect.any(String),
+          groupName: group.groupName,
+          adminIds: [
+            admin._id,
+          ],
+          devsInGroup: [devs[0]._id, devs[1]._id],
+          __v: 0
+        });
+      });
+  });
 
 
 });
