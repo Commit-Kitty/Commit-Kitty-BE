c80ab0c5dfcc3478cf01254ea133d37c43aed18f
Tess <52361640+tess-jl@users.noreply.github.com>
2020-02-24 10:13:15 -0800

    Card 6 group model (#5)
    * Group model drafted
    
    * Dev model drafted
    
    * test for groupName and adminId required fields is passing
    
    * test for custom validator for our devsInGroup field is passing
    
    * test for subdocument required field is passing
    
    * tests for Dev model written and passing
    
    * scaffolding for group routes
    
    * prepping to TDD group routes
    
    * seed.js updated to prepare for Group and Dev tests
    
    * hotfix of seed.js
    
    * added Dev and Group to data-helpers getters
    
    * ready to TDD POST route for Group model
    
    * miss me with that cookie, test for group model POST is passing
    
    * ready to TDD for Dev CREATE route
    
    * test for Dev CREATE passing
    
    * ready to TDD Dev get by id route
    
    * Dev model GET by id route test is passing

---
+++ __tests__/dev.test.js
@@ -0,0 +1,46 @@
+require('dotenv').config();
+const { getDev, devAgent } = require('../lib/helpers/data-helpers');
+const request = require('supertest');
+const app = require('../lib/app');
+
+
+describe('tests of dev model routes', () => {
+  it('can create a dev with ensureAuth only (either Dev or Admin)', () => {
+    return devAgent
+      .post('/api/v1/dev/')
+      .send({
+        devName: 'great group',
+        devGitHubHandle: '@githubHandle',
+      })
+      .then(res => {
+        expect(res.body).toEqual({
+          _id: expect.any(String),
+          devName: 'great group',
+          devGitHubHandle: '@githubHandle',
+          __v: 0
+        });
+      });
+  });
+
+  it('can get a dev by id with ensureAuth only (either Dev or Admin)', async() => {
+    const dev = await getDev();
+
+    return devAgent
+      .get(`/api/v1/dev/${dev._id}`)
+      .send({
+        devName: dev.devName,
+        devGitHubHandle: dev.devGitHubHandle,
+      })
+      .then(res => {
+        expect(res.body).toEqual({
+          _id: expect.any(String),
+          devName: dev.devName,
+          devGitHubHandle: dev.devGitHubHandle,
+          __v: 0
+        });
+      });
+  });
+  
+
+
+});
---
+++ __tests__/group.test.js
@@ -0,0 +1,32 @@
+require('dotenv').config();
+const { getGroup, getUser, getDevs, adminAgent } = require('../lib/helpers/data-helpers');
+const request = require('supertest');
+const app = require('../lib/app');
+
+
+describe('tests of group model routes', () => {
+  it('can create a group with admin access only ', async() => {
+    const admin = await getUser({ email: 'admin0@tess.com' });
+    const devs = await getDevs();
+
+    return adminAgent
+      .post('/api/v1/group/')
+      .send({
+        groupName: 'great group',
+        adminId: admin._id,
+        devsInGroup: [devs[0]._id, devs[1]._id]
+      })
+      .then(res => {
+        expect(res.body).toEqual({
+          _id: expect.any(String),
+          groupName: 'great group',
+          adminId: admin._id.toString(),
+          devsInGroup: [devs[0]._id.toString(), devs[1]._id.toString()],
+          __v: 0
+        });
+      });
+  });
+  
+
+
+});
